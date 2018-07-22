fa3655321d55c943ef0728ff114aa4e24e989295
zhanglei <zl@zhanglei.local>
2018-07-22 22:46:36 +0800

    start up

---
+++ suanfa/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  suanfa
+//
+//  Created by zhanglei on 2018/7/22.
+//  Copyright © 2018年 zhanglei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
