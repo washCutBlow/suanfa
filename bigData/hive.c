7e9fe0b1a0ded98d8fa3118dd9e5a1831e9ab0f5
zhanglei <zhanglei061@ke.com>
2019-06-25 17:55:17 +0800

    add

---
+++ bigData/hive.c
@@ -0,0 +1,9 @@
+//
+//  hive.c
+//  suanfa
+//
+//  Created by zhanglei on 2019/6/25.
+//  Copyright © 2019年 zhanglei. All rights reserved.
+//
+
+
---
+++ bigData/hive.h
@@ -0,0 +1,14 @@
+//
+//  hive.h
+//  suanfa
+//
+//  Created by zhanglei on 2019/6/25.
+//  Copyright © 2019年 zhanglei. All rights reserved.
+//
+
+#ifndef hive_h
+#define hive_h
+
+#include <stdio.h>
+
+#endif /* hive_h */
